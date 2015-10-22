5f650631bb41a2dda2af4430e3f165edd62cc841
Alex Taylor <alexmctaylorpants@gmail.com>
2015-10-22 12:57:14 -0700

    Moves possible_moves from knight.rb into piece.rb

--- lib/knight.rb
+++ lib/knight.rb
@@ -20,21 +20,5 @@ class Knight < Piece
                         ]
   end
 
-  def possible_moves
-    # - calculates all the possible target positions on the board
-    #     based on the piece's current position
-    # - communicates with the board to eliminate invalid moves
-    current_position = position
-    all_possible_moves = []
-
-    @possible_offsets.each do |pos|
-      target_pos = coord_add(current_position, pos)
-      all_possible_moves << target_pos
-    end
-
-    all_possible_moves
-  end
-
-
 
 end
--- lib/piece.rb
+++ lib/piece.rb
@@ -7,11 +7,32 @@ class Piece
   def initialize(game)
     @game     = game
     @position = [-1,-1]
+    @possible_offsets = []
   end
 
   def add_to_board_at(pos_x, pos_y)
     @game.board.add_piece self, pos_x, pos_y
     @position = [pos_x, pos_y]
   end
+
+  def possible_moves
+  # - calculates all the possible target positions on the board
+  #     based on the piece's current position
+  # - communicates with the board to eliminate invalid moves
+  current_position = position
+  all_possible_moves = []
+
+  @possible_offsets.each do |pos|
+    target_pos = coord_add(current_position, pos)
+
+    if target_pos[0] > 0 && target_pos[1] > 0
+
+      all_possible_moves << target_pos
+    end
+  end
+
+  all_possible_moves
+end
+
 
 end
