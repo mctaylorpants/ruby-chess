287987771f7b8ea7a480a6309368437ba4180069
Alex Taylor <alexmctaylorpants@gmail.com>
2015-10-22 13:12:09 -0700

    Fixes a bug in ChessHelpers::coord_add that was modifying the first array passed into it

--- lib/chess_helpers.rb
+++ lib/chess_helpers.rb
@@ -1,5 +1,7 @@
 module ChessHelpers
   def coord_add(arr_1, arr_2)
-    arr_1.each_with_index { |element, index| arr_1[index] += arr_2[index] }
+    arr = []
+    arr_1.each_with_index { |element, index| arr << arr_1[index] + arr_2[index] }
+    arr # matey!
   end
 end
--- lib/piece.rb
+++ lib/piece.rb
@@ -16,17 +16,20 @@ class Piece
   end
 
   def possible_moves
-  # - calculates all the possible target positions on the board
-  #     based on the piece's current position
-  # - communicates with the board to eliminate invalid moves
+  # - calculates all the possible target positions based on the piece's current
+  #     position
+  #
+  # - this filters out values less than 1, for obvious reasons, but it will not
+  #     filter out other coordinates that may be off the board. this should be
+  #     the board's responsibility since it knows things like how big it is,
+  #     as well as what other pieces may be in the way
   current_position = position
   all_possible_moves = []
-
+  byebug
   @possible_offsets.each do |pos|
     target_pos = coord_add(current_position, pos)
 
     if target_pos[0] > 0 && target_pos[1] > 0
-
       all_possible_moves << target_pos
     end
   end
