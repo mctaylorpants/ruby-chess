65ece69e6d9b4cabda3b3c75e6fa3aa7049ba401
Alex Taylor <alexmctaylorpants@gmail.com>
2015-10-22 15:46:54 -0700

    Fixes coordinate bug in board.rb

--- board.rb
+++ board.rb
@@ -1,6 +1,7 @@
 require "./nil_piece.rb"
 
 class Board
+  include ChessHelpers
   attr_reader :pieces
 
   # TODO: best practice for constants, is it OK to use them in the class?
@@ -9,7 +10,6 @@ class Board
 
   def initialize
     build_board
-
   end
 
   def add_piece(piece, pos_arr)
@@ -17,13 +17,17 @@ class Board
     #  starting from 1, we have to subtract one to the coordinates before we
     #  can access the correct element in the array.
     pos_x, pos_y = pos_arr
-    pieces[array_pos_for pos_x][array_pos_for pos_y] = piece
+
+    # remember, when storing in our array, we store by y,x, not by x,y.
+    pieces[array_pos_for pos_y][array_pos_for pos_x] = piece
   end
 
   def piece_at(pos_arr)
     # returns the piece at this position, or nil if it's empty.
     pos_x, pos_y = pos_arr
-    pieces[array_pos_for pos_x][array_pos_for pos_y]
+
+    # remember, in the array, we store by y,x, not by x,y.
+    pieces[array_pos_for pos_y][array_pos_for pos_x]
   end
 
   def possible_moves_for(this_piece)
@@ -49,7 +53,7 @@ class Board
   private
   def build_board
     # - our board is a multi-dimensional array where each outer element is a
-    #     row (x), and each inner element is a column (y).
+    #     column (y), and each inner element is a row (x).
     # - the objects are stored in the columns, but we set them to nil
     #     until we add pieces
     n = NilPiece.new(nil, nil) # see the NilPiece class for details
@@ -67,9 +71,5 @@ class Board
               ]
   end
 
-  def array_pos_for(i)
-    # offsets the coordinate position to return the correct element in the array.
-    i - 1
-  end
 
 end
