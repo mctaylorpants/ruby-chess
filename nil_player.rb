9c1ceb567da316a67978bca454e6da6ff7607361
Alex Taylor <alexmctaylorpants@gmail.com>
2015-10-23 15:24:58 -0700

    Adds nil player class

---
+++ nil_player.rb
@@ -0,0 +1,6 @@
+require "./player.rb"
+
+class NilPlayer < Player
+  # this class provides a 'nil' player object to use when a nil
+  #   piece is trying to figure out its owner (see display.rb)
+end
