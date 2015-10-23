7ff21b50e21b5c92f4bdd1537a1423a7cf5429c7
Alex Taylor <alexmctaylorpants@gmail.com>
2015-10-22 17:38:16 -0700

    Adds board display!    - the board displays, not pretty but it works    - TODO: implement keyboard commands    - TODO: implement the display of move options

--- display.rb
+++ display.rb
@@ -3,20 +3,48 @@ class Display
 
   def initialize(board:)
     # pass Display a board object so it can read it
+    @buffer = []
     @board = board
   end
 
   def update
+    buffer_fill
+    buffer_print
+  end
+
+  def icon_for(piece_type)
+    # serves up a string icon for each piece.
+    case piece_type
+      when nil;       "[    ]"
+      when :rook;     "[ RR ]"
+      when :knight;   "[ KN ]"
+      when :bishop;   "[ BI ]"
+      when :king;     "[ ++ ]"
+      when :queen;    "[ QQ ]"
+      when :pawn;     "[ pa ]"
+    end # case
+
+  end
+
+  def buffer_fill
     # even though this is the whole board, it's best to think of it as
     #   'row' because we're iterating through each row, working on column
     #   values one by one.
-    row = @board.pieces
-    byebug
-    row.each do |col|
-      col.each do |piece|
-        print " [#{piece.type}] "
+    @buffer = @board.pieces.map do |row|
+      r = []
+      row.each do |piece|
+        r << icon_for(piece.type)
       end
-    end
 
+      r
+    end # end @buffer=
+
+  end
+
+  def buffer_print
+    # prints the current buffer to the screen
+    @buffer.reverse.each do |line|
+      puts line.join("")
+    end
   end
 end
