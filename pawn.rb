4a25975da7976ea0a8288b0438c1983fbadc87b2
Alex Taylor <alexmctaylorpants@gmail.com>
2015-10-24 14:27:29 -0700

    Adds possible offsets for pawn

--- pawn.rb
+++ pawn.rb
@@ -10,8 +10,7 @@ class Pawn < Piece
     #  position of the piece. the resulting target coordinates will be
     #  validated by the board to make sure it wouldn't be off the board.
     @possible_offsets = [
-                          # [-2,1],
-                          # [-1,2],
+                          [0,1]
                         ]
   end
 
